f24ef10426ffa528af9227ece182d0fe529b317f
Riaz Moola <riaz.moola@fluidic.com>
2024-02-27 07:26:06 +0000

    basic working with FAUST text

--- ebooks.py
+++ ebooks.py
@@ -1,8 +1,10 @@
-import random;
 import sys;
 import pdb;
-
 import codecs;
+import tkinter as tk
+import time
+from random import seed, choice, randrange
+from string import ascii_letters
 
         
 
@@ -36,24 +38,25 @@ def generate_sentence(table, startwords):
 
     sentence = []
     entropy = 0
-    (w1, w2) = random.choice(startwords)
+    (w1, w2) = choice(startwords)
 
     for i in (w1, w2):
         sentence.append(i)
 
-    while len(" ".join(sentence)) < random.randrange(40, 130, 1):
+    while len(" ".join(sentence)) < randrange(40, 130, 1):
         try:
             selection = table[(w1, w2)]
         except KeyError:
             break
-        newword = random.choice(selection)
+        newword = choice(selection)
         entropy += len(selection)
         if newword in stopsentence:
             sentence[-1] = sentence[-1]+newword
         else:
             sentence.append(newword)
         w1, w2 = w2, newword
-    print(entropy, len(sentence))
+    print(entropy, sentence)
+    
     return (" ".join(sentence), entropy/len(sentence))
 
 def generate_table(input):
@@ -95,7 +98,7 @@ def generate_table(input):
 #     return None
 
 user = sys.argv[1]
-entropy = 1 
+
 try:
     input = open(user, 'r')
 except IOError:
@@ -103,7 +106,27 @@ except IOError:
 
 table, startwords = generate_table(input)
 
-while entropy <= 1:
-    tweet, entropy = generate_sentence(table, startwords)
-print(tweet)
 
+seed(42)
+
+colors = ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')
+def do_stuff():
+    entropy = 1 
+    while entropy <= 1:
+        tweet, entropy = generate_sentence(table, startwords)
+
+    s = tweet
+    color = choice(colors)
+    l.config(text=s, fg=color)
+    root.after(750, do_stuff)
+
+root = tk.Tk()
+root.wm_overrideredirect(True)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+root.bind("<Button-1>", lambda evt: root.destroy())
+
+l = tk.Label(text='', font=("Helvetica", 60))
+l.pack(expand=True)
+
+do_stuff()
+root.mainloop()
