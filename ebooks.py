45ea03687f2e4ae9f430e0a11826581d858ccddb
riaz <riazmoola@gmail.com>
2024-03-09 17:47:15 +0000

    made bot a class

--- ebooks.py
+++ ebooks.py
@@ -6,105 +6,69 @@ import time
 from random import seed, choice, randrange
 from string import ascii_letters
 
-        
-
-# def get_statuses(user):
-#     statuses = []
-#     max_id = None
-#     total = 0
-#     while True:
-#         timeline = api.GetUserTimeline(user, count=75, max_id=max_id)
-#         newCount = ignCount = 0
-#         for s in timeline:
-#                 statuses.append(s.text)
-#                 newCount += 1
-#         total += newCount
-# # "Fetched %d/%d new/total." % (
-#  #           newCount, total)
-#         if newCount == 0:
-#             break
-#         max_id = min([s.id for s in timeline]) - 1
-    
-#     statusfile = open('/home/r/Projects/ebooks/'+user, 'w')
-#     for i in statuses:
-#         statusfile.write(i.encode('utf-8')+'\n')
-#     statusfile.close()
-
-#     statuses = open('/home/r/Projects/ebooks/'+user, 'r')
-# return statuses
-
-def generate_sentence(table, startwords):
-    stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
-
-    sentence = []
-    entropy = 0
-    (w1, w2) = choice(startwords)
-
-    for i in (w1, w2):
-        sentence.append(i)
-
-    while len(" ".join(sentence)) < randrange(40, 130, 1):
-        try:
-            selection = table[(w1, w2)]
-        except KeyError:
-            break
-        newword = choice(selection)
-        entropy += len(selection)
-        if newword in stopsentence:
-            sentence[-1] = sentence[-1]+newword
-        else:
-            sentence.append(newword)
-        w1, w2 = w2, newword
-    print(entropy, sentence)
-    
-    return (" ".join(sentence), entropy/len(sentence))
-
-def generate_table(input):
-    stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
-    
-# GENERATE TABLE
+class markov_bot:
     table = {}
     startwords = []
+    def generate_sentence(self):
+        stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
     
-    for statustext in input:
-        w1, w2 = "\n", "\n"
-        # build an array of sentence beginnings:
-        try:
-            startwords.append((statustext.split()[0], statustext.split()[1]))
-        except IndexError:
-            startwords.append((statustext, " "))
-
-        for word in statustext.split():
-            if w1.istitle():
-                startwords.append((w1,w2))
-            if word[-1] in stopsentence:
-                table.setdefault( (w1, w2), [] ).append(word[0:-1])
-                w1, w2 = w2, word[0:-1]
-                word = word[-1]
+        sentence = []
+        entropy = 0
+        (w1, w2) = choice(self.startwords)
+    
+        for i in (w1, w2):
+            sentence.append(i)
+    
+        while len(" ".join(sentence)) < randrange(40, 130, 1):
+            try:
+                selection = self.table[(w1, w2)]
+            except KeyError:
+                break
+            newword = choice(selection)
+            entropy += len(selection)
+            if newword in stopsentence:
+                sentence[-1] = sentence[-1]+newword
+            else:
+                sentence.append(newword)
+                w1, w2 = w2, newword
+                print(entropy, sentence)
                 
-            table.setdefault( (w1, w2), [] ).append(word)
-            w1, w2 = w2, word
-    return (table, startwords)
+        return (" ".join(sentence), entropy/len(sentence))
+    
+    def generate_table(self, input):
+        stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
+        
+    # GENERATE TABLE
+        for statustext in input:
+            w1, w2 = "\n", "\n"
+            # build an array of sentence beginnings:
+            try:
+                self.startwords.append((statustext.split()[0], statustext.split()[1]))
+            except IndexError:
+                self.startwords.append((statustext, " "))
+    
+            for word in statustext.split():
+                if w1.istitle():
+                    self.startwords.append((w1,w2))
+                if word[-1] in stopsentence:
+                    self.table.setdefault( (w1, w2), [] ).append(word[0:-1])
+                    w1, w2 = w2, word[0:-1]
+                    word = word[-1]
+                    
+                self.table.setdefault( (w1, w2), [] ).append(word)
+                w1, w2 = w2, word
+        return True
 
-# def post_tweets(user, message):
-#     ebooks_cfg_parser = ConfigParser.ConfigParser()
-#     ebooks_cfg_parser.read('/home/r/Projects/ebooks/ebooks.cfg')
-#     user_key = ebooks_cfg_parser.get(user, 'access_token_key')
-#     user_secret = ebooks_cfg_parser.get(user, 'access_token_secret')
 
-#     status = api.PostUpdate(message)
-# #    print status.text
-#     return None
-
-user = sys.argv[1]
+source = sys.argv[1]
 
 try:
-    input = open(user, 'r')
+    input = open(source, 'r')
 except IOError:
-    print("error")
+    print("error, couldn't open the source text")
 
-table, startwords = generate_table(input)
-
+bot = markov_bot()
+bot.generate_table(input)
 
 seed(42)
 
@@ -112,7 +76,7 @@ colors = ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')
 def do_stuff():
     entropy = 1 
     while entropy <= 1:
-        tweet, entropy = generate_sentence(table, startwords)
+        tweet, entropy = bot.generate_sentence()
 
     s = tweet
     color = choice(colors)
