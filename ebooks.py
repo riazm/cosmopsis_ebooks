6d68e0fa3ba39f428f8e63b5b9e828cc4a1b77c0
riaz <riazmoola@gmail.com>
2024-03-10 16:24:45 +0000

    roughly working tbh

--- ebooks.py
+++ ebooks.py
@@ -42,39 +42,40 @@ class markov_bot:
     def generate_table(self, input):
         stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
         
-    # GENERATE TABLE
+        # GENERATE TABLE
         for statustext in input:
-            w1, w2 = "\n", "\n"
-            # build an array of sentence beginnings:
-            try:
-                self.startwords.append((statustext.split()[0],
-                                        statustext.split()[1]))
-            # sentence with less than 2 words
-            except IndexError:
-                pass
-    
-            for word in statustext.split():
-                if w1.istitle():
-                    self.startwords.append((w1,w2))
-                if word[-1] in stopsentence:
-                    self.table.setdefault( (w1, w2), [] ).append(word[0:-1])
-                    w1, w2 = w2, word[0:-1]
-                    word = word[-1]
-            
-                self.table.setdefault( (w1, w2), [] ).append(word)
-                w1, w2 = w2, word
+            self.add_input(statustext)
         return True
 
-    def add_input(self,input):
-        pass
+    def add_input(self, input):
+        stopsentence = ('.', '!', '?',) # Cause a "new sentence" if found at the end of a word
+        w1, w2 = "\n", "\n"
+        # build an array of sentence beginnings:
+        try:
+            self.startwords.append((input.split()[0],
+                                    input.split()[1]))
+            # sentence with less than 2 words
+        except IndexError:
+            pass
+            
+        for word in input.split():
+            if w1.istitle():
+                self.startwords.append((w1,w2))
+            if word[-1] in stopsentence:
+                self.table.setdefault( (w1, w2), [] ).append(word[0:-1])
+                w1, w2 = w2, word[0:-1]
+                word = word[-1]
+                    
+            self.table.setdefault( (w1, w2), [] ).append(word)
+            w1, w2 = w2, word
 
 
 def do_stuff():
     colors = ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')    
     entropy = 1 
     # only update if we managed to get somethnig that's new
-    while entropy <= 1:
-        tweet, entropy = bot.generate_sentence()
+    #while entropy <= 1:
+    tweet, entropy = bot.generate_sentence()
 
     s = tweet
     color = choice(colors)
@@ -90,7 +91,6 @@ def check_for_updates():
         print("found " +str(prompts-prev_prompts)+ " new prompts, adding to bot")
         for prompt in range(prev_prompts, prompts+1):
             prompt_text = requests.get("http://209.250.230.169:8000/myapp/prompt/"+str(prompt)).text
-            print(prompt_text, prompt)
             bot.add_input(prompt_text)
     prev_prompts = prompts 
 
